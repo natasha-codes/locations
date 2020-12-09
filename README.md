db6b2266a1de364278584e87e9f8db2f7014898a
Nathan Shelly <nsnshelly@gmail.com>
2020-12-08 22:27:49 -0800

    add Nix flake

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # locations
 
+## developing locally
+
+### server
+
+```bash
+> nix run .
+
+# or if you want to develop on the server itself
+
+# will create a nix shell with all required dependencies
+> nix develop
+
+# from launched nix shell
+[nix-shell:/path/to/this/repo/server]$ cargo run
+```
+
 ## Game Plan
 
 Sasha:
---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "locations - find your friends";
+
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nmattia/naersk";
+    mozillapkgs = {
+      url = "github:mozilla/nixpkgs-mozilla";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, utils, naersk, mozillapkgs }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages."${system}";
+
+        # get our specific rust version
+        mozilla = pkgs.callPackage (mozillapkgs + "/package-set.nix") {};
+        rust = (
+          mozilla.rustChannelOf {
+            date = "2020-12-07";
+            channel = "nightly";
+            sha256 = "sha256-MXTXVVKZ1H1LG4Y+SlKMHjVv/HI6U8bkCooNYI7Hhl0=";
+          }
+        ).rust;
+
+        # override the version used in naersk
+        naersk-lib = naersk.lib."${system}".override {
+          cargo = rust;
+          rustc = rust;
+        };
+      in
+        rec {
+          # `nix build`
+          packages.server = naersk-lib.buildPackage {
+            pname = "locations";
+            root = ./server;
+          };
+          defaultPackage = packages.server;
+
+          # `nix run`
+          apps.server = utils.lib.mkApp {
+            drv = packages.server;
+          };
+          defaultApp = apps.server;
+
+          # `nix develop`
+          devShell = pkgs.mkShell {
+            # supply the specific rust version
+            nativeBuildInputs = with pkgs; [ rust ];
+          };
+        }
+    );
+}
