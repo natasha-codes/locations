e3693b45eaa04bfd7a666670bc79132f2c0c6455
sashaweiss <asashaweiss@gmail.com>
2020-12-08 22:00:36 -0800

    Print fake locations

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,12 +2,35 @@
 
 #[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
+
+mod location;
+
+use location::*;
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite()
+        .mount("/", routes![get_a_location])
+        .launch();
 }
 
 #[get("/hello/<name>")]
-fn hello(name: String) -> String {
-    format!("Hello, {}", name)
+fn get_a_location(name: String) -> String {
+    let location = Location {
+        lat: Coordinate {
+            hour: 1,
+            min: 2,
+            second: 3,
+        },
+        long: Coordinate {
+            hour: 4,
+            min: 5,
+            second: 6,
+        },
+    };
+
+    format!("Hello, {}. You are at: {}", name, location)
 }
