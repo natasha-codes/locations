afb8c5adb28a03a10f434dde1f3036506a8a943d
sashaweiss <asashaweiss@gmail.com>
2020-12-11 23:32:02 -0800

    New app skeleton

---
+++ ios/Locations/LocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationsApp.swift
+//  Locations
+//
+//  Created by Sasha Weiss on 12/11/20.
+//
+
+import SwiftUI
+
+@main
+struct LocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
