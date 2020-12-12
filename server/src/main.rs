784c5f2052a9870360d077c31fd8aa9bdfb09e1a
sashaweiss <asashaweiss@gmail.com>
2020-12-11 23:56:42 -0800

    Add method to download MSA OpenID keys

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,23 +1,34 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+extern crate jsonwebtoken;
+extern crate reqwest;
 #[macro_use]
 extern crate rocket;
 extern crate rocket_contrib;
 #[macro_use]
 extern crate serde_derive;
+extern crate tokio;
 
 use rocket_contrib::json::Json;
 
+mod authentication;
 mod location;
 mod user;
 
 use location::{Coordinate, Location};
 use user::User;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let key_set = authentication::get_msa_key_set().await;
+
+    println!("{:?}", key_set);
+
+    /*
     rocket::ignite()
         .mount("/", routes![get_a_location])
         .launch();
+    */
 }
 
 #[get("/hello")]
