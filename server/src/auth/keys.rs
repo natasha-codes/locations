0f2dd2f80f736ca44aab5706909efdcba12398d0
sashaweiss <asashaweiss@gmail.com>
2020-12-12 16:02:34 -0800

    WIP - compiler is crashing so committing to submit a bug report

--- server/src/auth/keys.rs
+++ server/src/auth/keys.rs
@@ -1,31 +1,51 @@
+use jsonwebtoken::crypto::sign;
 use serde::Deserialize;
+
+use crate::auth::openid::JWT;
 
 #[derive(Debug, Deserialize)]
 pub struct KeySet {
-  keys: Vec<Key>,
+    keys: Vec<Key>,
 }
 
 impl KeySet {
-  pub fn new() -> Self {
-    Self { keys: vec![] }
-  }
+    pub fn new() -> Self {
+        Self { keys: vec![] }
+    }
+
+    pub fn validate_jwt(&self, jwt: &JWT) -> Option<bool> {
+        match self.key_with_id(jwt.id) {
+            Some(signing_key) => Some(signing_key.validate(jwt.signature)),
+            None => None,
+        }
+    }
+
+    fn key_with_id(&self, id: &str) -> Option<&Key> {
+        self.keys.iter().find(|key| key.thumbprint == id)
+    }
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Key {
-  #[serde(rename(deserialize = "kty"))]
-  pub key_type: String,
+    #[serde(rename(deserialize = "kty"))]
+    pub key_type: String,
 
-  #[serde(rename(deserialize = "kid"))]
-  pub thumbprint: String,
+    #[serde(rename(deserialize = "kid"))]
+    pub thumbprint: String,
 
-  #[serde(rename(deserialize = "x5c"))]
-  x509_certs: Vec<String>,
+    #[serde(rename(deserialize = "x5c"))]
+    x509_certs: Vec<String>,
 }
 
 impl Key {
-  pub fn key_data(&self) -> &String {
-    // The first value in this array is the key to be used for token verification
-    &self.x509_certs[0]
-  }
+    pub fn validate(&self, signature: String) -> bool {
+        // do jwt validation stuff
+
+        true
+    }
+
+    pub fn key_data(&self) -> &String {
+        // The first value in this array is the key to be used for token verification
+        &self.x509_certs[0]
+    }
 }
--- server/src/auth/openid.rs
+++ server/src/auth/openid.rs
@@ -1,44 +1,91 @@
-use std::time;
+use std::time::{Duration, Instant};
 
+use jsonwebtoken::Validation;
 use reqwest;
 use serde::Deserialize;
+use tokio::sync::Mutex;
 
 use crate::auth::keys::KeySet;
 
 pub struct OpenIDValidator {
-  key_set: KeySet,
-  last_refreshed_keyset: time::Instant,
+    cached_key_set: Mutex<CachedKeySet>,
+}
+
+struct CachedKeySet {
+    key_set: KeySet,
+    last_updated: Instant,
+}
+
+pub struct JWT {
+    pub id: String,
+    pub signature: String,
+}
+
+pub enum ValidationError {
+    UnknownKey,
+    FailedToValidate,
 }
 
 impl OpenIDValidator {
-  pub fn new() -> Self {
-    Self {
-      key_set: KeySet::new(),
-      last_refreshed_keyset: time::Instant::now(),
+    pub async fn new() -> Result<Self, reqwest::Error> {
+        let fresh_key_set = OpenIDValidator::get_fresh_msa_key_set().await?;
+
+        Ok(Self {
+            cached_key_set: Mutex::new(CachedKeySet {
+                key_set: fresh_key_set,
+                last_updated: Instant::now(),
+            }),
+        })
     }
-  }
 
-  async fn get_msa_key_set() -> Result<KeySet, reqwest::Error> {
-    // See: https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-protocols-oidc#fetch-the-openid-connect-metadata-document
+    pub async fn validate(&self, jwt: JWT) -> Result<(), ValidationError> {
+        let mut guard = self.cached_key_set.lock().await;
 
-    // Fill in "consumers" below because our MSA app is only accessible by personal MSAs.
-    let msa_authority = format!("https://login.microsoftonline.com/{}/v2.0/", "consumers");
-    let discovery_suffix = ".well-known/openid-configuration";
+        // If we know about the key used to sign this JWT, validate it.
+        if let Some(is_valid) = guard.key_set.validate_jwt(&jwt) {
+            return if is_valid {
+                Ok(())
+            } else {
+                Err(ValidationError::FailedToValidate)
+            };
+        }
 
-    let openid_metadata_url = msa_authority + discovery_suffix;
+        // Otherwise, if the cache is >5m old try and update it.
+        if Instant::now().duration_since(guard.last_updated) > Duration::from_secs(5 * 60) {
+            if let Ok(fresh_key_set) = OpenIDValidator::get_fresh_msa_key_set().await {
+                guard.key_set = fresh_key_set
+            }
 
-    let keys_uri = reqwest::get(&openid_metadata_url)
-      .await?
-      .json::<OpenIDMetadata>()
-      .await?
-      .key_roster_uri;
+            // Even if we failed to get a fresh key set above, set the updated time
+            // so we don't try again for another 5m.
+            guard.last_updated = Instant::now();
 
-    reqwest::get(&keys_uri).await?.json::<KeySet>().await
-  }
+            Ok(())
+        } else {
+            Err(ValidationError::UnknownKey)
+        }
+    }
+
+    async fn get_fresh_msa_key_set() -> Result<KeySet, reqwest::Error> {
+        // See: https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-protocols-oidc#fetch-the-openid-connect-metadata-document
+
+        // Fill in "consumers" below because our MSA app is only accessible by personal MSAs.
+        let msa_authority = "https://login.microsoftonline.com/consumers/v2.0";
+        let discovery_suffix = ".well-known/openid-configuration";
+        let openid_metadata_url = format!("{}/{}", msa_authority, discovery_suffix);
+
+        let keys_uri = reqwest::get(&openid_metadata_url)
+            .await?
+            .json::<OpenIDMetadata>()
+            .await?
+            .key_roster_uri;
+
+        reqwest::get(&keys_uri).await?.json::<KeySet>().await
+    }
 }
 
 #[derive(Deserialize)]
 struct OpenIDMetadata {
-  #[serde(rename(deserialize = "jwks_uri"))]
-  key_roster_uri: String,
+    #[serde(rename(deserialize = "jwks_uri"))]
+    key_roster_uri: String,
 }
